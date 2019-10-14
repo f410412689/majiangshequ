edf65f207450117730e81a26500aff68e4b5287d
付豪 <410412689@qq.com>
2019-10-14 09:35:53 +0800

    init repo

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class DemoController {
+    @GetMapping("/")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
+    }
+}
